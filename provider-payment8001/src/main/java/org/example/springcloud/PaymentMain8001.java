a13b82c68a1e3d14c04aafdfc5963224c8a96392
SilentLolex <silentrolex@gmail.com>
2022-03-12 19:18:11 +0800

    project init

---
+++ provider-payment8001/src/main/java/org/example/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package org.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author silentLolex
+ * @create 2022/3/12
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
