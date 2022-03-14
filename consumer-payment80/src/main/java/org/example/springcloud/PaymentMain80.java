9e476052fd54fa833b2caf5e232607acc998d682
SilentLolex <silentrolex@gmail.com>
2022-03-15 00:27:26 +0800

    add eureka-server and register services

--- consumer-payment80/src/main/java/org/example/springcloud/PaymentMain80.java
+++ consumer-payment80/src/main/java/org/example/springcloud/PaymentMain80.java
@@ -2,12 +2,14 @@ package org.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: SilentLolex
  * @Date: 2022/3/14 0:22
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain80 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain80.class,args);
--- eureka-server7001/src/main/java/org/example/springcloud/EurekaServer.java
+++ eureka-server7001/src/main/java/org/example/springcloud/EurekaServer.java
@@ -2,16 +2,16 @@ package org.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author silentLolex
- * @create 2022/3/12
+ * @create 2022/3/14
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 }
